773eb230301f50d5cc6001cf286d7c97ad83357c
CRgarcia15 <cg58708@gmial.com>
2022-06-10 14:14:22 -0400

    started js

---
+++ script.js
@@ -0,0 +1,11 @@
+/* make each topic into a button 
+href to the questions page 
+use an event listener for click and create a function for each one 
+so that the correct questions are displayed and the add it to the event listener
+*/
+
+const button1 = document.getElementById("cars").addEventListener("click", carsTopic());
+
+function carsTopic () {
+console.log('cars')
+}
