36b9eca4c58f9a6e2212882956ab650f2eec129a
CRgarcia15 <cg58708@gmial.com>
2022-06-10 19:04:58 -0400

    added some js code but not working

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ use an event listener for click and create a function for each one
 so that the correct questions are displayed and the add it to the event listener
 */
 
-const button1 = document.getElementById("cars").addEventListener("click", carsTopic());
+let button1 = document.getElementById("cars").addEventListener("click", carsTopic);
 
 function carsTopic () {
-console.log('cars')
+    location.href="question.html"
 }
