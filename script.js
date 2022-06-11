2807ecd7a0768beb959980c8c15535fb3c57db15
CRgarcia15 <cg58708@gmial.com>
2022-06-11 13:30:54 -0400

    fisxed issue with working with onclick

--- script.js
+++ script.js
@@ -4,8 +4,43 @@ use an event listener for click and create a function for each one
 so that the correct questions are displayed and the add it to the event listener
 */
 
-let button1 = document.getElementById("cars").addEventListener("click", carsTopic);
-
-function carsTopic () {
-    location.href="question.html"
+function topicCars () {
+    console.log('hi')
 }
+
+function topicBasketball () {
+    console.log('I')
+}
+
+function topicCoding () {
+    console.log('am')
+}
+
+function topicMovies () {
+    console.log('working')
+}
+
+function topicInternet () {
+    console.log('internet')
+}
+
+function topicSportHistory () {
+    console.log('Jordan the goat')
+}
+
+function topicFood () {
+    console.log('rice')
+}
+
+function topicCountries () {
+    console.log('DR')
+}
+
+function topicAnimals () {
+    console.log('dog')
+}
+
+function topicRandomFacts () {
+    console.log('random fact')
+}
+
